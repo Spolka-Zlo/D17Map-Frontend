cedb27064f940711fc1351def77d52be68c1db73
Radosław Niżnik <radekniznik57@gmail.com>
2024-04-24 13:15:33 +0200

    add simple routing

---
+++ src/app/calendar/page.tsx
@@ -0,0 +1,7 @@
+export default function Reservation() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            Calendar page
+        </main>
+    )
+}
