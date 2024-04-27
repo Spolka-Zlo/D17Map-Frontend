24a8414e334574d4d3785db27d479ea650ca35ae
Radosław Niżnik <radekniznik57@gmail.com>
2024-04-27 13:01:02 +0200

    make code prettier

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,9 +1,7 @@
-
-
 export default function Map() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            Map page
-        </main>
-    )
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Map page
+    </main>
+  );
 }
