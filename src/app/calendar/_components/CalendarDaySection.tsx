ed6ef461f8b732a0aea03c3f059863301d703b74
irosikoni <108950456+irosikoni@users.noreply.github.com>
2024-05-27 19:18:21 +0200

    calendar timetable base finished (#8)
    * calendar timetable base finished
    
    * opening fixed

--- src/app/calendar/_components/CalendarDaySection.tsx
+++ src/app/calendar/_components/CalendarDaySection.tsx
@@ -1,13 +1,24 @@
 import { Dispatch, SetStateAction } from "react";
+import { CalendarTimeTable } from "./CalendarTimeTable";
+import { Reservation } from "@/app/calendar/page";
 
 type CalendarDaySectionProps = {
   isOpen?: boolean;
   setIsOpen?: Dispatch<SetStateAction<boolean>>;
+  reservations: Reservation[];
+  room: string;
 };
 
+//isOpen and setIsOpen prepared for future use
 export function CalendarDaySection({
-  isOpen,
+  isOpen = true,
   setIsOpen,
+  reservations,
+  room,
 }: CalendarDaySectionProps) {
-  return <div></div>;
+  return (
+    <div className="bg-white w-full grow pl-10">
+      <CalendarTimeTable reservations={reservations} room={room} />
+    </div>
+  );
 }
--- src/app/calendar/_components/CalendarSection.tsx
+++ src/app/calendar/_components/CalendarSection.tsx
@@ -3,42 +3,53 @@ import { Calendar } from "@/components/Calendar";
 import { useState } from "react";
 import { CalendarTimeManager } from "./CalendarTimeManager";
 import { CalendarDaySection } from "./CalendarDaySection";
+import { Reservation } from "@/app/calendar/page";
 
-type ReservationType = "Lecture" | "Consultation" | "Exam";
-
-type Reservation = {
-  title: string;
-  startTime: string;
-  endTime: string;
-  date: Date;
+type CalendarSectionProps = {
+  reservations: Reservation[];
+  availableRooms: string[];
 };
 
-export function CalendarSection() {
-  const [selectedDate, setSelectedDate] = useState<Date | undefined>(
-    new Date()
-  );
+export function CalendarSection({
+  reservations,
+  availableRooms,
+}: CalendarSectionProps) {
+  const [selectedDate, setSelectedDate] = useState<Date>(new Date());
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedRoom, setSelectedRoom] = useState("room");
+
   return (
-    <section className="flex">
-      <div className="grid grid-cols-2 maxSM:grid-cols-1 maxSM:grid-rows-2 rounded-lg">
+    <section className="flex w-full justify-between gap-10">
+      <div className="grid grid-cols-2 maxSM:grid-cols-1 maxSM:grid-rows-2 rounded-lg w-full h-fit grow-0">
         <Calendar
           mode="single"
           selected={selectedDate}
           onDayClick={(day) => {
-            setIsOpen(
-              !selectedDate || selectedDate.getDay === day.getDay
-                ? !isOpen
-                : true
-            );
+            setIsOpen(selectedDate.getDay() === day.getDay() ? !isOpen : true);
             setSelectedDate(day);
           }}
           disabled={{ before: new Date() }}
           className="z-10 rounded-lg"
         />
 
-        <CalendarTimeManager isOpen={isOpen} setIsOpen={setIsOpen} />
+        <CalendarTimeManager
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          selectedRoom={selectedRoom}
+          setSelectedRoom={setSelectedRoom}
+          availableRooms={availableRooms}
+        />
       </div>
-      <CalendarDaySection isOpen={isOpen} setIsOpen={setIsOpen} />
+
+      <CalendarDaySection
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        reservations={reservations.filter(
+          (reservation) =>
+            reservation.date.toDateString === selectedDate.toDateString
+        )}
+        room={selectedRoom}
+      />
     </section>
   );
 }
