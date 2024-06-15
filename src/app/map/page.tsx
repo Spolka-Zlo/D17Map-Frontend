585d6af46a78f984ae5632eccfacf7896eae7ba0
irosikoni <108950456+irosikoni@users.noreply.github.com>
2024-06-15 22:03:20 +0200

    D17 90/map model floor 1&2 (#10)
    * model addedgit add .!
    
    * trying to move pieces
    
    * room info added
    
    * now it looks nice
    
    * change of floor is possible now
    
    * map works

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,3 +1,16 @@
+import { MapSection } from "./_components/MapSection";
+
+// let this empty map description be here for now
 export default function Map() {
-  return <main>Map page</main>;
+  return (
+    <main>
+      <div className="w-full flex flex-row justify-stretch">
+        <MapSection />
+        <div>
+          <h1 className="text-2xl font-bold">Map</h1>
+          <p>Map description</p>
+        </div>
+      </div>
+    </main>
+  );
 }
