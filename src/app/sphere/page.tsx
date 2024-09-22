b2a9668968dd05726f8601c5d487ae97ac65ea0c
irosikoni <irosikoni@gmail.com>
2024-09-22 13:56:19 +0200

    spheric-photos

--- src/app/sphere/page.tsx
+++ src/app/sphere/page.tsx
@@ -1,8 +1,12 @@
+import Frame from "./_components/Frame";
+import { Rig } from "./_components/Rig";
 import ThreeSixtyViewer from "./_components/ThreeSixtyViewer";
 
 export default function Sphere() {
   return (
     <main>
+      <Frame id={"0"} name={""} author={""} bg={""} children={undefined} />
+      <Rig />
       <div className="w-[70vw]">
         <h1>360 Street View</h1>
         <ThreeSixtyViewer />
