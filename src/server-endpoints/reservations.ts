e1c289e75a0557fea688620070862c877d1cd5ee
irosikoni <irosikoni@gmail.com>
2024-12-31 01:57:17 +0100

    fix build

--- src/server-endpoints/reservations.ts
+++ src/server-endpoints/reservations.ts
@@ -1,7 +1,7 @@
-import { getBuildingName } from "@/auth/getBuildingName";
 import { HOST } from "./host";
+import { cookies } from "next/headers";
 
-const buildingName = await getBuildingName();
+const buildingName = cookies().get("buildingName")?.value || "D17";
 
 export const ADD_RESERVATION_URL = `${HOST}/buildings/${buildingName}/reservations`;
 export const ADD_RECURRING_RESERVATION_URL = `${HOST}/buildings/${buildingName}/reservations/recurringReservations`;
